4f4c254010486763ffb0ade7f1d63cb5573d03c9
SpaceIm <30052553+SpaceIm@users.noreply.github.com>
2020-08-10 09:56:10 +0200

    add reentrant option and components

--- conanfile.py
+++ conanfile.py
@@ -6,7 +6,7 @@ from conans import ConanFile, CMake, tools
 class QhullConan(ConanFile):
     name = "qhull"
     description = "Qhull computes the convex hull, Delaunay triangulation, " \
-                  "Voronoi diagram, halfspace intersection  about a point, " \
+                  "Voronoi diagram, halfspace intersection about a point, " \
                   "furthest-site Delaunay triangulation, and furthest-site " \
                   "Voronoi diagram."
     license = "Qhull"
@@ -16,8 +16,16 @@ class QhullConan(ConanFile):
     exports_sources = ["CMakeLists.txt", "patches/**"]
     generators = "cmake"
     settings = "os", "arch", "compiler", "build_type"
-    options = {"shared": [True, False], "fPIC": [True, False]}
-    default_options = {"shared": False, "fPIC": True}
+    options = {
+        "shared": [True, False],
+        "fPIC": [True, False],
+        "reentrant": [True, False]
+    }
+    default_options = {
+        "shared": False,
+        "fPIC": True,
+        "reentrant": True
+    }
 
     _cmake = None
 
@@ -38,6 +46,9 @@ class QhullConan(ConanFile):
             del self.options.fPIC
         del self.settings.compiler.libcxx
         del self.settings.compiler.cppstd
+
+    def package_id(self):
+        del self.info.options.reentrant
 
     def source(self):
         tools.get(**self.conan_data["sources"][self.version])
@@ -69,12 +80,43 @@ class QhullConan(ConanFile):
     def package_info(self):
         self.cpp_info.names["cmake_find_package"] = "Qhull"
         self.cpp_info.names["cmake_find_package_multi"] = "Qhull"
-        self.cpp_info.libs = tools.collect_libs(self)
+        self.cpp_info.components["libqhull"].names["cmake_find_package"] = self._qhull_cmake_name
+        self.cpp_info.components["libqhull"].names["cmake_find_package_multi"] = self._qhull_cmake_name
+        self.cpp_info.components["libqhull"].names["pkg_config"] = self._qhull_lib_name
+        self.cpp_info.components["libqhull"].libs = [self._qhull_lib_name]
         if self.settings.os == "Linux":
-            self.cpp_info.system_libs.append("m")
+            self.cpp_info.components["libqhull"].system_libs.append("m")
         if self.settings.compiler == "Visual Studio" and self.options.shared:
-            self.cpp_info.defines.extend(["qh_dllimport"])
+            self.cpp_info.components["libqhull"].defines.extend(["qh_dllimport"])
 
         bin_path = os.path.join(self.package_folder, "bin")
         self.output.info("Appending PATH environment variable: {}".format(bin_path))
         self.env_info.PATH.append(bin_path)
+
+    @property
+    def _qhull_cmake_name(self):
+        libname = ""
+        if self.settings.build_type == "Debug":
+            if self.options.shared:
+                libname = "qhull_rd" if self.options.reentrant else "qhull_d"
+            else:
+                libname = "qhullstatic_rd" if self.options.reentrant else "qhullstatic_d"
+        else:
+            if self.options.shared:
+                libname = "qhull_r" if self.options.reentrant else "libqhull"
+            else:
+                libname = "qhullstatic_r" if self.options.reentrant else "qhullstatic"
+        return libname
+
+    @property
+    def _qhull_lib_name(self):
+        libname = "qhull"
+        if not self.options.shared:
+            libname += "static"
+        if self.settings.build_type == "Debug" or self.options.reentrant:
+            libname += "_"
+            if self.options.reentrant:
+                libname += "r"
+            if self.settings.build_type == "Debug":
+                libname += "d"
+        return libname
